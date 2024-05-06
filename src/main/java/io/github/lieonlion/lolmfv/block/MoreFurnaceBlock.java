c6fa999018bca929c5f58af6c08c5591576a613d
lieonlion <lieonlion4@gmail.com>
2024-05-06 13:34:45 +0100

    1.0.1

--- src/main/java/io/github/lieonlion/lolmfv/block/MoreFurnaceBlock.java
+++ src/main/java/io/github/lieonlion/lolmfv/block/MoreFurnaceBlock.java
@@ -7,45 +7,37 @@ import net.minecraft.stats.Stats;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FurnaceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.function.ToIntFunction;
 
 public class MoreFurnaceBlock extends FurnaceBlock {
     public String type;
 
     public MoreFurnaceBlock(MapColor colour, float strength, String type) {
-        super(Properties.of().mapColor(colour).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(strength).lightLevel(litFurnaceLightLevel()));
+        super(Properties.ofFullCopy(Blocks.FURNACE).mapColor(colour).strength(strength));
         this.type = type;
     }
     public MoreFurnaceBlock(MapColor colour, SoundType sound, float strength, String type) {
-        super(Properties.of().mapColor(colour).sound(sound).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(strength).lightLevel(litFurnaceLightLevel()));
+        super(Properties.ofFullCopy(Blocks.FURNACE).mapColor(colour).sound(sound).strength(strength));
         this.type = type;
-    }
-
-    private static ToIntFunction<BlockState> litFurnaceLightLevel() {
-        return (blockState) -> {
-            return (Boolean) blockState.getValue(BlockStateProperties.LIT) ? 13 : 0;
-        };
     }
 
     @Override
     @Nullable
-    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NotNull Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> type) {
         return MoreFurnaceBlock.createFurnaceTicker(level, type, BlockInit.MORE_FURNACE_BLOCK_ENTITY.get());
     }
 
     @Override
-    protected void openContainer(Level level, BlockPos pos, Player player) {
+    protected void openContainer(Level level, @NotNull BlockPos pos, @NotNull Player player) {
         BlockEntity blockEntity = level.getBlockEntity(pos);
         if (blockEntity instanceof MoreFurnaceBlockEntity) {
             player.openMenu((MenuProvider)blockEntity);
@@ -55,11 +47,7 @@ public class MoreFurnaceBlock extends FurnaceBlock {
 
     @Nullable
     @Override
-    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+    public BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
         return new MoreFurnaceBlockEntity(pos, state);
-    }
-
-    public String getType() {
-        return type;
     }
 }
